801061b1a431b07e095fb023a0bcdb806aa2b8b1
Kelner Roberto <kelner250@hotmail.com>
2022-05-19 15:37:53 -0300

    component: creating new file pageGeneral of login to wrap everything inside it and be able to modify it freely if needed

---
+++ application/src/components/login/FormCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Flex } from "@chakra-ui/layout";
+
+const FormCard = () => {
+  <Flex>
+    
+  </Flex>
+}
+
+export default FormCard;
---
+++ application/src/components/login/PageGeneral.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Flex } from "@chakra-ui/layout";
+import FormCard from "./FormCard";
+
+const PageGeneral = () => {
+  <Flex height='100vh' alignItems='center' justifyContent='center'>
+    <FormCard />
+  </Flex>
+}
+
+export default PageGeneral;
