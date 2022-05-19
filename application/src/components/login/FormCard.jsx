a5ed120ffe8b87212084191d24bd1b2733d629b2
Kelner Roberto <kelner250@hotmail.com>
2022-05-19 15:58:02 -0300

    component/style: creates the login component using chakraUi style - feat: darkmode for login page - missing: create states to save the typed email and password - login button to verify condition of states and if its correct redirect to mainpage

--- application/src/components/login/FormCard.jsx
+++ application/src/components/login/FormCard.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Flex } from "@chakra-ui/layout";
+import { Flex, Heading } from "@chakra-ui/layout";
+import { Input } from "@chakra-ui/input";
+import { Button } from "@chakra-ui/button";
+import { useColorMode, useColorModeValue } from "@chakra-ui/color-mode";
 
 const FormCard = () => {
-  <Flex>
-    
-  </Flex>
+  const { toggleColorMode } = useColorMode();
+  const formBg = useColorModeValue("gray.100", "gray.700");
+  return(
+    <Flex direction='column' background={formBg} p={12} rounded={6}>
+      <Heading mb={6}>Log in</Heading>
+      <Input placeholder="email@email.com" variant='filled' mb={3} type='email'/>
+      <Input placeholder="*********" variant='filled' mb={6} type='password' />
+      <Button colorScheme='teal' mb={6}>Log in</Button>
+      <Button onClick={toggleColorMode}>Dark mode?</Button>
+    </Flex>
+  )
 }
 
 export default FormCard;
--- application/src/components/login/PageGeneral.jsx
+++ application/src/components/login/PageGeneral.jsx
@@ -3,9 +3,11 @@ import { Flex } from "@chakra-ui/layout";
 import FormCard from "./FormCard";
 
 const PageGeneral = () => {
-  <Flex height='100vh' alignItems='center' justifyContent='center'>
-    <FormCard />
-  </Flex>
+  return(
+    <Flex height='100vh' alignItems='center' justifyContent='center'>
+      <FormCard />
+    </Flex>
+  )
 }
 
 export default PageGeneral;
